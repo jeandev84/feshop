681e82af04eb185fef38139f998e120cbb9f2559
janklod84 <jeanyao34@yahoo.com>
2019-07-10 17:11:19 +0500

    User Registration End

--- app/views/layouts/luxury.php
+++ app/views/layouts/luxury.php
@@ -248,6 +248,7 @@ $currency = \Framework\App::$app->get('currency');
 
 <script src="js/jquery-1.11.0.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+<script src="js/validator.js"></script>
 <script src="js/typeahead.bundle.js"></script>
 <script src="js/jquery.easydropdown.js"></script>
 
